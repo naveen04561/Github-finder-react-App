a501cac81423b5ef486187e3e446e06cdef6c815
Naveen <72813093+naveen04561@users.noreply.github.com>
2023-03-03 15:52:41 +0000

    Second Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+        <div>
+            <h1 className="text-6xl mb-4">Github Finder</h1>
+            <p>Hello! This app provides details of users from Github.</p>
+        </div>
+    )
+}
+
+export default About;
